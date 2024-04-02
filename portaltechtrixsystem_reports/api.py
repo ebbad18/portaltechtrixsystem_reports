fe32e5f8d227d1277e81c399aa5ed24b4076b08a
safdar211ali <safdar211@hotmail.com>
2024-04-02 14:10:28 +0500

    upated

--- portaltechtrixsystem_reports/api.py
+++ portaltechtrixsystem_reports/api.py
@@ -3,4 +3,4 @@ import frappe
 
 @frappe.whitelist()
 def get_items():
-    return frappe.get_all("Item", fields=["item_name", "item_group", "last_purchase_rate"])
+    return frappe.get_all("Item", fields=["item_name", "item_group", "last_purchase_rate","image"])
